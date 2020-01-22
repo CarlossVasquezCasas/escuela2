d25b2f4dc0996e90f8b556cd2d11975ec3b94deb
carloss hammerlyb vasquez casas <carlos_9_71_45@hotmail.com>
2020-01-22 15:42:23 -0500

    subiendo

--- empleado-ms/src/main/java/com/everis/escuela/Controller/EmpleadoController.java
+++ empleado-ms/src/main/java/com/everis/escuela/Controller/EmpleadoController.java
@@ -1,27 +1,17 @@
 package com.everis.escuela.Controller;
 
-import javax.validation.Valid;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.everis.escuela.dto.EmpleadoDTO;
 import com.everis.escuela.entidad.Empleado;
 import com.everis.escuela.exceptions.ResourceNotFoundException;
-import com.everis.escuela.exceptions.ValidationException;
 import com.everis.escuela.service.EmpleadoService;
 import com.everis.escuela.service.impl.FeignServiceImp;
-
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 
 @RefreshScope
